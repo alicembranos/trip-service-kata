e2e7c3b332d79643d5499af30fbad5ed11bbfe66
alicia <alicembru15@hotmail.com>
2024-03-31 21:28:17 +0200

    build: install xdebug in docker image to run tests coverage

---
+++ php/src/TripServiceKata/Trip/TestableTripService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\TripServiceKata\Trip;
+
+use TripServiceKata\Trip\TripService;
+
+class TestableTripService extends TripService
+{
+    protected function getLoggedUser()
+    {
+        return null;
+    }
+}
